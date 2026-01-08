73bc43126052f6e6450f312cfbfd95d3268628c9
Mark Harvey Dacara <medacara@student.apc.edu.ph>
2026-01-08 13:08:51 +0800

    revert back

--- introduction/js/script.js
+++ introduction/js/script.js
@@ -1,9 +1,38 @@
+// DOM interaction (Dark mode toggle)
+document.getElementById("themeBtn").addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+});
+
+// Vue Gallery App
 Vue.createApp({
   data() {
     return {
-      pictures: [
-        { name: "My First Project", url: "images/kokushibo.png" }
+      images: [
+        { name: "Me 1", url: "images/me1.jpg" },
+        { name: "Me 2", url: "images/me2.jpg" },
+        { name: "Me 3", url: "images/me3.jpg" }
       ]
     }
   }
 }).mount("#galleryApp");
+
+// Vue Guestbook App
+Vue.createApp({
+  data() {
+    return {
+      name: "",
+      message: "",
+      comments: []
+    }
+  },
+  methods: {
+    addComment() {
+      this.comments.push({
+        name: this.name,
+        message: this.message
+      });
+      this.name = "";
+      this.message = "";
+    }
+  }
+}).mount("#guestbookApp");
