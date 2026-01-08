f0a45db9eb7130204c3969967d6e1e5d6007483a
Mark Harvey Dacara <medacara@student.apc.edu.ph>
2026-01-08 14:51:55 +0800

    removed few images

--- introduction/js/script.js
+++ introduction/js/script.js
@@ -8,11 +8,9 @@ Vue.createApp({
   data() {
     return {
       images: [
-        { name: "Profile", url: "images/profile.jpg" },
         { name: "Project 1", url: "images/img1.jpg" },
         { name: "Project 2", url: "images/img2.jpg" },
         { name: "Project 3", url: "images/img3.jpg" },
-        { name: "Project 4", url: "images/img4.jpg" }
       ]
     };
   }
