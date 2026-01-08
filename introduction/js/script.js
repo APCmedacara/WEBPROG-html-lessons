5eda71a3b8dcd184ec5547dcd90de0a0ebc855d3
Mark Harvey Dacara <medacara@student.apc.edu.ph>
2026-01-08 14:49:33 +0800

    try

--- introduction/js/script.js
+++ introduction/js/script.js
@@ -3,15 +3,16 @@ document.getElementById("themeBtn").addEventListener("click", () => {
   document.body.classList.toggle("dark");
 });
 
-// Vue Gallery App
+// Vue Gallery
 Vue.createApp({
   data() {
     return {
       images: [
-        { name: "Profile Pic", url: "images/profile.jpg" },
+        { name: "Profile", url: "images/profile.jpg" },
         { name: "Project 1", url: "images/img1.jpg" },
         { name: "Project 2", url: "images/img2.jpg" },
-        { name: "Project 3", url: "images/img3.jpg" }
+        { name: "Project 3", url: "images/img3.jpg" },
+        { name: "Project 4", url: "images/img4.jpg" }
       ]
     };
   }
