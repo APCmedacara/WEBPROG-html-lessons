763ca640b55d89a14f6f0088b1da346212d237d0
Mark Harvey Dacara <medacara@student.apc.edu.ph>
2026-01-08 15:00:55 +0800

    few changes here and there

--- introduction/js/script.js
+++ introduction/js/script.js
@@ -8,10 +8,29 @@ Vue.createApp({
   data() {
     return {
       images: [
-        { name: "Project 1", url: "images/img1.jpg" },
-        { name: "Project 2", url: "images/img2.jpg" },
-        { name: "Project 3", url: "images/img3.jpg" },
+        { name: "Project 1", url: "images/img1.jpg", link: "gallery1.html" },
+        { name: "Project 2", url: "images/img2.jpg", link: "gallery2.html" },
+        { name: "Project 3", url: "images/img3.jpg", link: "gallery3.html" }
       ]
     };
+  },
+  mounted() {
+    // Make gallery images clickable
+    this.$nextTick(() => {
+      document.querySelectorAll('.clickable').forEach(el => {
+        el.addEventListener('click', () => {
+          const link = el.dataset.link;
+          if (link) window.location.href = link;
+        });
+      });
+    });
   }
 }).mount("#galleryApp");
+
+// Make About Cards clickable
+document.querySelectorAll('.card.clickable').forEach(card => {
+  card.addEventListener('click', () => {
+    const link = card.dataset.link;
+    if (link) window.location.href = link;
+  });
+});
