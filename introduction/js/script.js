bd567a01a863b064553ce778d1222015a444ed15
Mark Harvey Dacara <medacara@student.apc.edu.ph>
2026-01-08 10:16:52 +0800

    updated using flexbox

--- introduction/js/script.js
+++ introduction/js/script.js
@@ -1,16 +1,11 @@
-document.getElementById("themeBtn").addEventListener("click", () => {
-  document.body.classList.toggle("dark-mode");
-});
-
-
 Vue.createApp({
   data() {
     return {
       pictures: [
-        { name: "Project 1", url: "images/kokushibo.png" },
-        { name: "Project 2", url: "images/project2.png" },
-        { name: "Project 3", url: "images/project3.png" },
-        { name: "Project 4", url: "images/project4.png" }
+        {
+          name: "My First Project",
+          url: "images/kokushibo.png"
+        }
       ]
     }
   }
